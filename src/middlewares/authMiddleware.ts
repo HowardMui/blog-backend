bc69398aa28d63d0f83f28e8f4386d4722b70414
HowardMui <howardmui.2020@gmail.com>
2023-06-05 00:30:31 +0800

    refactor: authenticateToken middleware for signin with password

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -3,32 +3,60 @@ import jwt from "jsonwebtoken";
 import { JWT_SECRET } from "../config";
 import moment from "moment";
 import { PrismaClient, User } from "@prisma/client";
+import { UserJWTAuth } from "../Model";
 
 type AuthRequest = Request & { user?: User };
 
 const prisma = new PrismaClient();
 
 export const authenticateToken = async (req: AuthRequest, res: Response, next: NextFunction) => {
-  const authHeader = req.headers["authorization"];
+  const userToken = req.cookies["token"];
 
-  const JWTtoken = authHeader?.split(" ")[1];
+  // const authHeader = req.headers["authorization"];
+  // const cookie = req.headers["cookie"];
 
-  if (!JWTtoken) {
+  // let newJWT: string | null = null;
+
+  // if (cookie) {
+  //   const newJWTTokenIndex =
+  //     cookie
+  //       .split(/\;|\=/)
+  //       .map((el) => el.trim())
+  //       .findIndex((el) => el === "token") + 1;
+  //   newJWT = cookie.split(/\;|\=/)[newJWTTokenIndex];
+  // }
+
+  // const JWTtoken = authHeader?.split(" ")[1];
+
+  if (!userToken) {
     return res.sendStatus(401);
   }
 
+  // console.log(newJWT);
+
   try {
-    const payload = (await jwt.verify(JWTtoken, JWT_SECRET)) as { tokenId: number };
-    const findDbToken = await prisma.token.findUnique({
-      where: { tokenId: payload.tokenId },
-      include: { user: true },
-    });
-    if (!findDbToken?.valid || !moment(findDbToken.expiration).isAfter(moment().toISOString())) {
+    const payload = (await jwt.verify(userToken, JWT_SECRET)) as UserJWTAuth;
+
+    const exp = moment(payload.exp! * 1000).toISOString();
+
+    if (!payload.userId) {
+      return res.status(401).json({ error: "UnAuthorized" });
+    } else if (!moment(exp).isAfter(moment().toISOString())) {
       return res.status(401).json({ error: "API token expired" });
     }
 
+    const findDbToken = await prisma.user.findUnique({
+      where: { userId: payload.userId },
+      // include: { user: true },
+    });
+
+    // console.log(findDbToken);
+    // if (!findDbToken?.valid || !moment(findDbToken.expiration).isAfter(moment().toISOString())) {
+    //   return res.status(401).json({ error: "API token expired" });
+    // }
+
     // add user properties into request
-    req.user = findDbToken.user;
+    // req.user = findDbToken.user;
   } catch (err) {
     return res.sendStatus(401);
   }
