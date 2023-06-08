3441a88200fede8bec0f96591517b96fe5c3853a
HowardMui <howardmui.2020@gmail.com>
2023-06-09 03:15:19 +0800

    refactor: rm cors and /api prefix

--- src/index.ts
+++ src/index.ts
@@ -4,17 +4,19 @@ import { tweetRouter } from "./routers/tweetRoutes";
 import swaggerDocs from "./utils/swagger";
 import { authRouter } from "./routers/authRoutes";
 import { authenticateToken } from "./middlewares/authMiddleware";
-// var cors = require("cors");
+var cors = require("cors");
 import cookieParser from "cookie-parser";
 
 const app = express();
 app.use(cookieParser());
+// app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 // app.use(cors({ credentials: true, origin: "*" }));
+// app.use(cors());
 app.use(express.json());
 
-app.use("/api/users", authenticateToken, userRouter);
-app.use("/api/tweets", tweetRouter);
-app.use("/api/auths", authRouter);
+app.use("/users", authenticateToken, userRouter);
+app.use("/tweets", tweetRouter);
+app.use("/auths", authRouter);
 
 app.get("/", (req, res) => {
   res.send("hello world server side");
